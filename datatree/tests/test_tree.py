2e00fb1cd3c80a3d702d6b44fd709946dbfd7aa9
Alexandre Andrade <ama@esss.com.br>
2015-04-06 12:13:53 -0300

    Moving test_tree.py to its right place.

--- datatree/tests/test_tree.py
+++ datatree/tests/test_tree.py
@@ -1,9 +1,27 @@
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
 
-from datatree import Tree
 
-class test_Tree(unittest.TestCase):
-    pass # TODO: Revisit. Does this need its own tests?
+
+def test_json():
+    from datatree import Tree
+
+    tree = Tree()
+    with tree.author() as author:
+        author.name('Terry Pratchett')
+        author.genre('Fantasy/Comedy')
+        author // 'Only 2 books listed'
+        with author.novels(count=2) as novels:
+            novels.novel('Small Gods', year=1992)
+            novels.novel('The Fifth Elephant', year=1999)
+            novels.novel("Guards! Guards!", year=1989)
+
+    assert tree.render('json', pretty=True, sort_keys=True) == '''{
+    "author": {
+        "name": "Terry Pratchett",
+        "genre": "Fantasy/Comedy",
+        "novels": [
+            "Small Gods",
+            "The Fifth Elephant",
+            "Guards! Guards!"
+        ]
+    }
+}'''
